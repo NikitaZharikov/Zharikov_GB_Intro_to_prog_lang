2c914abbc081db1750426b24bed53cd3500c190b
Nikita_Zharikov <nikitkazharikov@gmail.com>
2023-01-20 09:14:11 +0300

    fix2

--- HW_2/task2/Program.cs
+++ HW_2/task2/Program.cs
@@ -6,13 +6,24 @@
 // 32679 -> 6
 
 Console.Write("Введите число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-int result = num1 % 10;
-if (num1 < 100)
+int num = Convert.ToInt32(Console.ReadLine());
+int result1 = num % 10;
+int result2 = (num / 10) % 10;
+int result3 = (num / 100) % 10;
+
+if (num < 100)
 {
-    Console.WriteLine("У числа " + num1 + " нет третьей цифры");
+    Console.WriteLine("У числа " + num + " нет третьей цифры");
+}
+else if (num >= 100 && num < 1000)
+{
+    Console.WriteLine("Третья цифра числа " + num + " будет " + result1);
+}
+else if (num > 1000 && num < 10000)
+{
+    Console.WriteLine("Третья цифра числа " + num + " будет " + result2);
 }
 else
 {
-    Console.WriteLine("Третья цифра числа " + num1 + " будет " + result);
+    Console.WriteLine("Третья цифра числа " + num + " будет " + result3);
 }
