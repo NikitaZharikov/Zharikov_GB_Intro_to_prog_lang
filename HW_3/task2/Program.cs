99066c265403b1f88ed4862115485e2aabd9be9b
Nikita_Zharikov <nikitkazharikov@gmail.com>
2023-01-23 13:47:39 +0300

    Fix 21

--- HW_3/task2/Program.cs
+++ HW_3/task2/Program.cs
@@ -3,28 +3,30 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-double lenghtXYZ(int x1, int x2, int x3, int y1, int y2, int y3)
+double lenghtBetweenDots(int dot_AX, int dot_AY, int dot_AZ, int dot_BX, int dot_BY, int dot_BZ)
 {
-    double xres = (x1 - x2 - x3) * (x1 - x2 - x3);
-    double yres = (y1 - y2 - y3) * (y1 - y2 - y3);
-    double lrez = Math.Sqrt(xres + yres);
-    return lrez;
+    double resX = Math.Pow((dot_BX - dot_AX), 2);
+    double resY = Math.Pow((dot_BY - dot_AY), 2);
+    double resZ = Math.Pow((dot_BZ - dot_AZ), 2);
+    double finalResult = Math.Sqrt((resX + resY + resZ));
+    return finalResult;
 }
-Console.WriteLine("Узнайте расстояние между точками в 3D пространстве!");
-Console.WriteLine("Введите координаты точки по оси X");
-Console.Write("Первое значение координаты точки x: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Второе значение координаты точки x: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Третье значение координаты точки x: ");
-int x3 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Узнайте расстояние между точками A и B в 3D пространстве!");
+Console.WriteLine("Введите координаты точки A");
+Console.Write("Координаты точки A по оси X: ");
+int dot_AX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Координаты точки B по оси Y: ");
+int dot_AY = Convert.ToInt32(Console.ReadLine());
+Console.Write("Координаты точки B по оси Z: ");
+int dot_AZ = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
-Console.WriteLine("Введите координаты точки по оси Y");
-Console.Write("Первое значение координаты точки y: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Второе значение координаты точки y: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Третье значение координаты точки y: ");
-int y3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B");
+Console.Write("Координаты точки B по оси X: ");
+int dot_BX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Координаты точки B по оси Y: ");
+int dot_BY = Convert.ToInt32(Console.ReadLine());
+Console.Write("Координаты точки B по оси Z: ");
+int dot_BZ = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
-Console.WriteLine("Расстояние между точками равно " + Math.Round(lenghtXYZ(x1, x2, x3, y1, y2, y3), 2));
+Console.WriteLine("Расстояние между точками равно " + Math.Round(lenghtBetweenDots(dot_AX, dot_AY, dot_AZ, dot_BX, dot_BY, dot_BZ), 2));
