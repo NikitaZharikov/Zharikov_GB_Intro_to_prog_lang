0c97d268a71b4a897d8b0039817f0c81fbb01d5a
NikitaZharikov <nikitkazharikov@gmail.com>
2023-01-25 23:21:42 +0300

    add code

--- HW_4/task2/Program.cs
+++ HW_4/task2/Program.cs
@@ -3,25 +3,44 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+
 Console.WriteLine("");
 Console.WriteLine("*****************************************");
 Console.WriteLine("Программа для сложения всех цифр в числе");
 Console.WriteLine("*****************************************");
 Console.WriteLine("");
 
-Console.Write("Введите число чтобы узнать сумму его цифр: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
+// Интерактивный код
+// Console.Write("Введите число чтобы узнать сумму его цифр: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
 
-void SumOfNumbs()
+// void SumOfNumbsInteract()
+// {
+//     while (num != 0)
+//     {
+//         sum += num % 10;
+//         num /= 10;
+//     }
+
+//     Console.WriteLine("Сумма всех цифр в числе равна: " + sum);
+// }
+
+// SumOfNumbsInteract();
+
+
+// Легкий код
+int SumOfNumbsLight(int num)
 {
+    int sum = 0;
     while (num != 0)
     {
         sum += num % 10;
         num /= 10;
     }
-
-    Console.WriteLine("Сумма всех цифр в числе равна: " + sum);
+    return sum;
 }
 
-SumOfNumbs();
+int result = SumOfNumbsLight(555);
+Console.WriteLine("Сумма всех цифр в числе равна: " + result);
